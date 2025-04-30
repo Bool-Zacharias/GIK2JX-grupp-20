47a57ce92c59e6358b00afe02689651bf73728c2
Bool-Zacharias <h23zacan@du.se>
2025-04-30 14:08:28 +0200

    dd

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
 import pandas as pd
 from flask import Flask, jsonify
 from sklearn.cluster import KMeans
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -13,13 +12,20 @@ def home():
 
 @app.route("/api/clusters")
 def api_clusters():
+    # Läs in CSV-filen
     df = pd.read_csv("static/data/school_locations.csv")
 
+    # Byt namn på kolumner
     df = df.rename(columns={"ycoord": "lat", "xcoord": "lon"})
 
+    # Skapa KMeans-kluster
     coords = df[["lat", "lon"]]
-    kMeans = KMeans(n_clusters=3, random_state=50).fit(coords)
-    df["clusters"] = kMeans.labels_
+    kMeans = KMeans(n_clusters=3, random_state=42).fit(coords)
+    df["cluster"] = kMeans.labels_
 
+    # Skicka tillbaka JSON-objekt
     return jsonify(df.to_dict(orient="records"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
