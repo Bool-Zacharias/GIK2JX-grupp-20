e674deb2220952cef494c0616c78928e21d083d5
Bool-Zacharias <h23zacan@du.se>
2025-05-03 12:22:49 +0200

    Zoom funciton

---
+++ static/js/zoomFunction.js
@@ -0,0 +1,7 @@
+function zoomFunctionSthlm (){
+    mymap.flyTo([59.29509272313594, 18.08190939127913],8)
+}
+
+function zoomFunctionFalun(){
+    mymap.flyTo([60.60606708750362, 15.632632737027222],10)
+}
